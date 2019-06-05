f3732b394ba6da0c1eac637cb9d466d5e2921405
Brian <brianelblog@gmail.com>
2019-06-05 03:56:16 -0300

    archivos faltantes

---
+++ src/fiuba/algo3/tp2/Material.java
@@ -0,0 +1,9 @@
+package fiuba.algo3.tp2;
+
+import fiuba.algo3.tp2.Herramienta;
+
+public class Material {
+    protected int durabilidad;
+    public void desgastar(Herramienta unaHerramienta) {
+    }
+}
