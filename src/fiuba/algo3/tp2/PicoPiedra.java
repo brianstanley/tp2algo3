85c783eab9976abe9bcaca9f5725d0c75f8e1121
sbs001 <tassone_af@yahoo.com>
2019-06-05 16:38:23 -0300

    Arreglo el detalle de los picos comentados por german

--- src/fiuba/algo3/tp2/PicoPiedra.java
+++ src/fiuba/algo3/tp2/PicoPiedra.java
@@ -4,7 +4,8 @@ import fiuba.algo3.tp2.estrategiasDesgaste.EstrategiaDesgasteDividida;
 
 public class PicoPiedra extends Pico {
     public PicoPiedra() {
+        this.durabilidad = 200;
         this.fuerza = 4;
-        this.desgastador = new EstrategiaDesgasteDividida(200, this.fuerza, 1.5);
+        this.desgastador = new EstrategiaDesgasteDividida(this.durabilidad, this.fuerza, 1.5);
     }
 }
