a2de9b1db8b594f8ec5c7877272ebd2db87c795d
germandawi <germandawi@gmail.com>
2019-06-07 21:01:13 -0300

    agregada funcionalidad para que los picos puedan daniar a las piedras, con pruebas correspondientes

--- src/fiuba/algo3/tp2/herramientas/picos/Pico.java
+++ src/fiuba/algo3/tp2/herramientas/picos/Pico.java
@@ -1,6 +1,12 @@
 package fiuba.algo3.tp2.herramientas.picos;
 
 import fiuba.algo3.tp2.herramientas.Herramienta;
+import fiuba.algo3.tp2.materiales.PiedraMaterial;
 
 public abstract class Pico extends Herramienta {
+
+    public void romper(PiedraMaterial unaPiedra){
+        unaPiedra.desgastarCon(this);
+        this.desgastador.desgastar();
+    }
 }
