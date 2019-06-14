d4e9f13c62e8c070c2a9c88733ed8626924ad6bd
germandawi <germandawi@gmail.com>
2019-06-14 10:18:17 -0300

    se termino de adaptar tests de herramientas para que reconozcan la excepcion HerramientaRota

--- test/fiuba/algo3/tp2/herramientasTest/PicoPiedraTest.java
+++ test/fiuba/algo3/tp2/herramientasTest/PicoPiedraTest.java
@@ -4,10 +4,18 @@ import fiuba.algo3.tp2.herramientas.Herramienta;
 import fiuba.algo3.tp2.herramientas.excepciones.HerramientaRotaExcepcion;
 import fiuba.algo3.tp2.herramientas.picos.PicoPiedra;
 import fiuba.algo3.tp2.materiales.MaderaMaterial;
+import fiuba.algo3.tp2.materiales.Material;
+import org.apache.ivy.core.report.MetadataArtifactDownloadReport;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class PicoPiedraTest {
+
+    public void usarHerramientaNVeces(int veces, Herramienta herramienta, Material material) {
+        for (int i = 0; i < veces; i++) {
+            herramienta.romper(material);
+        }
+    }
 
     @Test
     public void SeCreaPicoPiedraExitosamente(){
@@ -32,6 +40,13 @@ public class PicoPiedraTest {
         double durabilidadReducidaPorUso = 4/1.5;
         picoPiedra.romper(unaMadera);
         Assert.assertEquals(picoPiedra.getDurabilidad(), durabilidadInicial - durabilidadReducidaPorUso, 000.4);
+    }
+
+    @Test (expected = HerramientaRotaExcepcion.class)
+    public void SeUsaPicoPiedraHastaAgotarSuDurabilidadYSelanzaExcepcion(){
+        PicoPiedra pico = new PicoPiedra();
+        MaderaMaterial material = new MaderaMaterial();
+        usarHerramientaNVeces(80, pico,material);
     }
 
 }
