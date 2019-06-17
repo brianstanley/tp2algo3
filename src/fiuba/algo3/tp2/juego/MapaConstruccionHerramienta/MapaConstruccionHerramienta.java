f2ef2da01c01c7fbc4c73908fd145d66f65bde29
sbs001 <tassone_af@yahoo.com>
2019-06-17 00:47:45 -0300

    Mapa de construccion de herramienta ya no usa Casilleros

--- src/fiuba/algo3/tp2/juego/MapaConstruccionHerramienta/MapaConstruccionHerramienta.java
+++ src/fiuba/algo3/tp2/juego/MapaConstruccionHerramienta/MapaConstruccionHerramienta.java
@@ -1,51 +1,47 @@
 package fiuba.algo3.tp2.juego.MapaConstruccionHerramienta;
 
-import fiuba.algo3.tp2.juego.Casillero;
+
 import fiuba.algo3.tp2.materiales.*;
 import fiuba.algo3.tp2.juego.MapaConstruccionHerramienta.Figuras.*;
-import org.junit.Test;
+
 
 public class MapaConstruccionHerramienta {
 
-    private Casillero[][] mapa;
+    private Material[][] mapa;
     private LectorDeMapa lector;
 
     public MapaConstruccionHerramienta() {
 
-        this.mapa = new Casillero[3][3];
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                mapa[i][j] = new Casillero();
-
+        this.mapa = new Material[3][3];
         this.lector = new LectorDeMapa();
     }
 
 
     public void insertarMaterialEnPosicion(Material unMaterial, int x, int y) {
 
-        this.mapa[x][y].almacenarMaterial(unMaterial);
+        this.mapa[x][y] = unMaterial;
     }
 
 
 
     public boolean hayMaderaEnPosicion(int x, int y) {
 
-        return this.mapa[x][y].getMaterialGuardado() instanceof MaderaMaterial;
+        return this.mapa[x][y]  instanceof MaderaMaterial;
     }
 
     public boolean hayPiedraEnPosicion(int x, int y) {
 
-        return this.mapa[x][y].getMaterialGuardado() instanceof PiedraMaterial;
+        return this.mapa[x][y] instanceof PiedraMaterial;
     }
 
     public boolean hayMetalEnPosicion(int x, int y) {
 
-        return this.mapa[x][y].getMaterialGuardado() instanceof MetalMaterial;
+        return this.mapa[x][y] instanceof MetalMaterial;
     }
 
     public boolean noHayMaterialEnPosicion(int x, int y) {
 
-        return this.mapa[x][y].getMaterialGuardado() == null;
+        return this.mapa[x][y] == null;
     }
 
 
