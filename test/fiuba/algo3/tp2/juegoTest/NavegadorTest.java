78cb366228971e49a68633def4c8246b244058ad
germandawi <germandawi@gmail.com>
2019-06-17 22:26:01 -0300

    agregado un test que chequea la utilidad del metodo agregado

---
+++ test/fiuba/algo3/tp2/juegoTest/NavegadorTest.java
@@ -0,0 +1,24 @@
+package fiuba.algo3.tp2.juegoTest;
+
+import fiuba.algo3.tp2.herramientas.picos.PicoPiedra;
+import fiuba.algo3.tp2.juego.ElementoDeCampo;
+import fiuba.algo3.tp2.juego.Mapa.Mapa;
+import fiuba.algo3.tp2.juego.Navegador.Navegador;
+import fiuba.algo3.tp2.juego.Navegador.Posicion;
+import fiuba.algo3.tp2.materiales.MaderaMaterial;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NavegadorTest {
+
+    @Test
+    public void elNavegadorDevuelveElContenidoDelCasilleroDeEnfente() {
+        Mapa mapa = new Mapa(5, 5);
+        Navegador navegador = new Navegador(3, 3, mapa);
+        Posicion posicionAlNorte = new Posicion(3, 2);
+        MaderaMaterial madera = new MaderaMaterial();
+        madera.ponerEnMapa(mapa, posicionAlNorte);
+        ElementoDeCampo elem = navegador.obtenerMaterialPosicionEnfrente();
+        Assert.assertEquals(madera.getClass(), elem.getClass());
+    }
+}
