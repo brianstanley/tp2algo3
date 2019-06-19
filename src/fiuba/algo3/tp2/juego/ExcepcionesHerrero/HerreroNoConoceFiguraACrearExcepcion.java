d33f37ea098160229c766cdecd28e6a830945070
sbs001 <tassone_af@yahoo.com>
2019-06-19 17:04:32 -0300

    exepcion de herrero

---
+++ src/fiuba/algo3/tp2/juego/ExcepcionesHerrero/HerreroNoConoceFiguraACrearExcepcion.java
@@ -0,0 +1,9 @@
+package fiuba.algo3.tp2.juego.ExcepcionesHerrero;
+
+public class HerreroNoConoceFiguraACrearExcepcion extends RuntimeException {
+
+    public HerreroNoConoceFiguraACrearExcepcion(String mensaje) {
+
+        super(mensaje);
+    }
+}
