1b8d82110b71304c698d2ae0a2eb7234f1055407
sbs001 <tassone_af@yahoo.com>
2019-06-25 00:24:12 -0300

    eliminanos container sin uso

--- test/fiuba/algo3/tp2/juegoTest/utilsTest/ItemsContainerTest.java
+++ test/fiuba/algo3/tp2/juegoTest/utilsTest/ItemsContainerTest.java
@@ -1,51 +1,9 @@
 package fiuba.algo3.tp2.juegoTest.utilsTest;
 
-import fiuba.algo3.tp2.juego.utils.MaterialesContainer;
 import fiuba.algo3.tp2.materiales.MetalMaterial;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ItemsContainerTest {
-    @Test
-    public void seAgrega3ItemsMetalAColleccion() {
-        MetalMaterial metal = new MetalMaterial();
-        MaterialesContainer materialesContainer = new MaterialesContainer();
 
-        materialesContainer.agregar(metal);
-        materialesContainer.agregar(metal);
-        materialesContainer.agregar(metal);
-
-        Assert.assertEquals(3, materialesContainer.getCantidadDe(metal));
-    }
-
-    @Test
-    public void seRemueveItemDeLaCollecionYDecrementaSuCantidad() {
-        MetalMaterial metal = new MetalMaterial();
-        MaterialesContainer materialesContainer = new MaterialesContainer();
-
-        materialesContainer.agregar(metal);
-        materialesContainer.agregar(metal);
-        materialesContainer.agregar(metal);
-
-        Assert.assertEquals(3, materialesContainer.getCantidadDe(metal));
-
-        materialesContainer.decrementarCantidadDe(metal);
-
-        Assert.assertEquals(2, materialesContainer.getCantidadDe(metal));
-    }
-
-    @Test
-    public void seRemueveClaveDeColeccion() {
-        MetalMaterial metal = new MetalMaterial();
-        MaterialesContainer materialesContainer = new MaterialesContainer();
-
-        materialesContainer.agregar(metal);
-        materialesContainer.agregar(metal);
-        materialesContainer.agregar(metal);
-
-        materialesContainer.remover(metal);
-
-        Assert.assertEquals(0, materialesContainer.getCantidadDe(metal));
-
-    }
 }
